a0b84febd69261f4716c7af9761c2534af09d75a
openhoangnc <hoangnc.jp@gmail.com>
2025-03-14 17:28:34 +0700

    refactor: Improve file handling and signal management in write test

--- main.go
+++ main.go
@@ -33,37 +33,68 @@ func main() {
 	rand.Read(randBytes)
 	filename := fmt.Sprintf("ssd-test-%x.tmp", randBytes)
 
-	f, err := os.Create(filename)
+	file, err := createTestFile(filename, fileSize)
 	if err != nil {
-		panic(err)
-	}
-
-	cleanup := func() {
-		fmt.Println("\nCleaning up...")
-		f.Close()
-		os.Remove(filename)
-		fmt.Println("Removed file", filename)
-	}
-
-	fmt.Printf("Writing %s to %s\n", formatBytes(fileSize), filename)
-	if err := f.Truncate(fileSize); err != nil {
-		fmt.Printf("Error allocating file %s with size %s: %s\n", filename, formatBytes(fileSize), err)
-		cleanup()
+		fmt.Println(err)
 		return
 	}
 
-	// handle signals
-	sign := make(chan os.Signal, 1)
-	signal.Notify(sign, os.Interrupt, syscall.SIGTERM)
-	stopped := false
-	go func() {
-		<-sign
-		stopped = true
-	}()
+	// Setup cleanup to be called on exit
+	cleanup := func() {
+		fmt.Println("\nCleaning up...")
+		file.Close()
+		os.Remove(filename)
+		fmt.Println("Removed file", filename)
+	}
+	defer cleanup()
 
+	// Setup signal handling for graceful termination
+	stopChan, signalReceived := setupSignalHandler()
+
+	// Prepare random data for writing
 	randData := make([]byte, 1*1024*1024)
 	rand.Read(randData)
 
+	// Perform the write test
+	performWriteTest(file, fileSize, randData, stopChan, signalReceived)
+}
+
+// createTestFile creates a file with the specified name and size
+func createTestFile(filename string, size int64) (*os.File, error) {
+	f, err := os.Create(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create file: %w", err)
+	}
+
+	fmt.Printf("Writing %s to %s\n", formatBytes(size), filename)
+	if err := f.Truncate(size); err != nil {
+		f.Close()
+		os.Remove(filename)
+		return nil, fmt.Errorf("error allocating file %s with size %s: %w",
+			filename, formatBytes(size), err)
+	}
+
+	return f, nil
+}
+
+// setupSignalHandler creates a channel that is closed when SIGINT or SIGTERM is received
+func setupSignalHandler() (<-chan struct{}, *bool) {
+	stopChan := make(chan struct{})
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	signalReceived := false
+
+	go func() {
+		<-signalChan
+		signalReceived = true
+		close(stopChan)
+	}()
+
+	return stopChan, &signalReceived
+}
+
+// performWriteTest writes random data to the file and displays progress
+func performWriteTest(f *os.File, fileSize int64, randData []byte, stopChan <-chan struct{}, signalReceived *bool) {
 	totalWritten := int64(0)
 	blockWritten := int64(0)
 	writeStart := time.Now()
@@ -72,57 +103,69 @@ func main() {
 
 	firstWrite := true
 	for {
-		if stopped || totalWritten >= fileSize {
-			break
-		}
-
-		written, err := f.Write(randData)
-
-		if err != nil {
-			if !strings.Contains(err.Error(), "no space left on device") {
-				fmt.Println("\nError writing to file:", err)
-			}
-			break
-		}
-
-		totalWritten += int64(written)
-		blockWritten += int64(written)
-
-		if time.Since(blockStart) > 1*time.Second {
-			if err := f.Sync(); err != nil {
-				fmt.Println("\nError syncing file:", err)
-				break
+		select {
+		case <-stopChan:
+			// Just exit the loop without another progress update when signal is received
+			return
+		default:
+			if totalWritten >= fileSize {
+				return
 			}
 
-			totalElapsed := time.Since(writeStart)
-			blockElapsed := time.Since(blockStart)
+			written, err := f.Write(randData)
 
-			blockSpeed := float64(blockWritten) / blockElapsed.Seconds()
-			if blockSpeed > maxSpeed {
-				maxSpeed = blockSpeed
+			if err != nil {
+				if !strings.Contains(err.Error(), "no space left on device") {
+					fmt.Println("\nError writing to file:", err)
+				}
+				return
 			}
-			avgSpeed := float64(totalWritten) / totalElapsed.Seconds()
 
-			s := "" +
-				"Written size  : " + padStr(formatBytes(totalWritten)) + "\n" +
-				"Elapsed time  : " + padStr(formatDuration(totalElapsed)) + "\n" +
-				"Average speed : " + padStr(formatBytes(int64(avgSpeed))) + "/s\n" +
-				"Current speed : " + padStr(formatBytes(int64(blockSpeed))) + "/s\n" +
-				"Max speed     : " + padStr(formatBytes(int64(maxSpeed))) + "/s\n"
+			totalWritten += int64(written)
+			blockWritten += int64(written)
 
-			if firstWrite {
-				firstWrite = false
-			} else {
-				fmt.Printf("\033[%dA", strings.Count(s, "\n"))
+			// Only update progress if no signal has been received
+			if time.Since(blockStart) > 1*time.Second && !*signalReceived {
+				updateProgress(f, totalWritten, blockWritten, writeStart,
+					blockStart, &maxSpeed, &firstWrite)
+				blockWritten = 0
+				blockStart = time.Now()
 			}
-			fmt.Print(s)
-
-			blockWritten = 0
-			blockStart = time.Now()
 		}
 	}
+}
 
-	cleanup()
+// updateProgress displays the current test progress
+func updateProgress(f *os.File, totalWritten, blockWritten int64,
+	writeStart, blockStart time.Time, maxSpeed *float64, firstWrite *bool) {
+
+	if err := f.Sync(); err != nil {
+		fmt.Println("\nError syncing file:", err)
+		return
+	}
+
+	totalElapsed := time.Since(writeStart)
+	blockElapsed := time.Since(blockStart)
+
+	blockSpeed := float64(blockWritten) / blockElapsed.Seconds()
+	if blockSpeed > *maxSpeed {
+		*maxSpeed = blockSpeed
+	}
+	avgSpeed := float64(totalWritten) / totalElapsed.Seconds()
+
+	s := "" +
+		"Written size  : " + padStr(formatBytes(totalWritten)) + "\n" +
+		"Elapsed time  : " + padStr(formatDuration(totalElapsed)) + "\n" +
+		"Average speed : " + padStr(formatBytes(int64(avgSpeed))) + "/s\n" +
+		"Current speed : " + padStr(formatBytes(int64(blockSpeed))) + "/s\n" +
+		"Max speed     : " + padStr(formatBytes(int64(*maxSpeed))) + "/s\n"
+
+	if *firstWrite {
+		*firstWrite = false
+	} else {
+		fmt.Printf("\033[%dA", strings.Count(s, "\n"))
+	}
+	fmt.Print(s)
 }
 
 func padStr(s string) string {
